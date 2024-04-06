fa9699cfcbc0954b75cc2a4d6a48217a5d6f1b99
hoda <hoda.nikpour@ntnu.no>
2024-04-07 02:04:05 +0330

    implement changes

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import FooterComponent from "./components/ui/Footer.component";
+import NavbarComponent from "./components/ui/Navbar.component";
+
+function App(){
+    return(
+        <div>
+            {/*navbar*/}
+            <NavbarComponent />
+
+
+            {/*wrapper*/}
+            <div className="wrapper">
+                <h2>wrapper</h2>
+            </div>
+
+
+            {/*footer*/}
+            <FooterComponent/>
+        </div>
+    );
+}
+    export default App;
+
