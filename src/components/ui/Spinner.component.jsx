5202a6aaad821865242b75697fb35eae3e5d42f0
hoda <hoda.nikpour@ntnu.no>
2024-04-07 22:46:12 +0330

    new3

---
+++ src/components/ui/Spinner.component.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import spinnerImg from '../../assets/images/spinner.gif'
+
+const Spinner = () => {
+    return(
+        <div className="d-flex justify-content-center align-items-center">
+       <img src={spinnerImg} alt="spinner"/>
+        </div>
+    );
+    
+};
+
+export default Spinner;
